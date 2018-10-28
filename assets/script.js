1e2aea25e7c5e6ab63fef9b5dfe343b2e02c6acf
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-10-29 00:22:37 +0100

    Adding is_on to local.storage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,32 @@
 'use strict'
 
 let is_on = true
+let is_on_obj = { 'is_on': is_on}
+
+if(localStorage.getItem('is_on') == null)
+	localStorage.setItem('is_on', JSON.stringify(is_on_obj))
+else
+	is_on = localStorage.getItem('is_on')
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	is_on = request.is_on 
+	
+	is_on_obj = { 'is_on': is_on}
+	localStorage.setItem('is_on', JSON.stringify(is_on_obj))
 })
+
+
+let selected_text = ""
+
+function get_the_selected_text() {
+	let text = ""
+	if (typeof window.getSelection != "undefined")
+		text = window.getSelection().toString()
+	else            
+		if (typeof document.selection != "undefined" && document.selection.type == "Text")
+			text = document.selection.createRange().text;
+
+	return text
+}
+
+
