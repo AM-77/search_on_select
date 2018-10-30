c8781a2132655995e6831c338f8c2a08d63d7acf
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-10-30 19:08:13 +0100

    Fixing the one click issue.

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@
 let is_on = true
 let is_on_obj = { 'is_on': is_on}
 
+console.log(localStorage.getItem('is_on'))
+
 if(localStorage.getItem('is_on') == null)
 	localStorage.setItem('is_on', JSON.stringify(is_on_obj))
 else
@@ -13,6 +15,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 	is_on_obj = { 'is_on': is_on}
 	localStorage.setItem('is_on', JSON.stringify(is_on_obj))
+	
+	console.log(localStorage.getItem('is_on'))
 })
 
 
@@ -28,7 +32,6 @@ function get_the_selected_text() {
 	return text
 }
 
-let div
 let Google = "https://www.google.dz/search?q="
 let Duckduckgo = "https://duckduckgo.com/?q="
 let Yahoo = "https://search.yahoo.com/search?p="
@@ -43,42 +46,44 @@ document.addEventListener("mouseup", function(e){
 			let encoded_selected_text = encodeURI(selected_text)
 			create_search_div(encoded_selected_text, e)
 			selected_text = ""
-/*
+
 			setTimeout(function(){
-				divs = document.querySelectorAll(".selected_text_extension")
-				for(let i = 0; i < divs.length; i++)
-					document.body.removeChild(divs[i])
-			}, 25000) */
+				if(document.querySelector(".selected_text_extension") != null)
+					document.body.removeChild(document.querySelector("div.selected_text_extension"))
+			}, 3000)
 		}
 	}
 	
-})
+}, false)
 
 document.addEventListener("keyup", function(e){
-	if(is_on){
+	console.log(document.querySelector("div.selected_text_extension") )
+	if(is_on && document.querySelector("div.selected_text_extension") == null ){
+
 		selected_text = get_the_selected_text()
 		if(selected_text != ""){
 			
 			let encoded_selected_text = encodeURI(selected_text)
 			create_search_div(encoded_selected_text, e)
 			selected_text = ""
-/*
+
 			setTimeout(function(){
-				divs = document.querySelectorAll(".selected_text_extension")
-				for(let i = 0; i < divs.length; i++)
-					document.body.removeChild(divs[i])
-			}, 25000) */
+				if(document.querySelector(".selected_text_extension") != null)
+					document.body.removeChild(document.querySelector("div.selected_text_extension"))
+			}, 3000) 
 		}
 	}
-})
+}, false)
+
 
 function create_search_div(encoded_selected_text, e){
+
 	let div = document.createElement("div")
 	div.classList.add("selected_text_extension")
-	div.setAttribute("style", "width: 200px;height: 35px;z-index: 10000; position: absolute; left: "+e.pageX+"px; top: "+e.pageY+"px; border: 1px solid rgb(226, 226, 226); border-radius: 3px; background: rgb(245, 245, 245);overflow: hidden;");
+	div.setAttribute("style", "width: 100%; height: 100%; background: rgba(0,0,0,0.5); z-index: 10000;position: absolute; left: 0px; top: 0px;");
 	
 	div.innerHTML=`
-		<ul style='list-style: none;width: 100%;height: 100%;padding: 0;margin: 0;' >
+		<ul style='list-style: none;width: 200px;height: 35px; position: absolute; left: ${e.pageX}px; top: ${e.pageY}px; border: 1px solid rgb(226, 226, 226); border-radius: 3px; background: rgb(245, 245, 245);overflow: hidden;padding: 0;margin: 0;' >
 			<li style='width: 20%;padding: 11px;margin: 0;float:left;box-sizing: border-box;'>
 				<a style='position: relative;top: -2px' target='blank' href="${ Google + encoded_selected_text } "> 
 					<img style="width: 100%; height: 100%;" src="${chrome.extension.getURL("/assets/logos/google.svg")}" />
@@ -113,15 +118,4 @@ function create_search_div(encoded_selected_text, e){
 }
 
 
-window.addEventListener("click", function(e){
-	let divs = document.querySelectorAll("div.selected_text_extension")
-
-	if(divs.length == 2){
-		document.body.removeChild(divs[1])
-		let div = divs[0]
-		if(div != null && (div != e.target || div != e.target.parentElement || div != e.target.parentElement.parentElement  || div != e.target.parentElement.parentElement.parentElement)){
-			document.body.removeChild(div)
-		}
-	}
-})
 
